cc20392b836fe6afa4fd59d0155df68effe6555c
kenken714 <kenkenkeiko043@gmail.com>
2020-07-09 23:36:49 +0900

    resolved #1

--- math/com.cpp
+++ math/com.cpp
@@ -1,22 +1,22 @@
 ll f[210000];
-void cominit() {
+void cominit(ll mod = 1e9+7) {
     f[0] = 1;
-    REP(i, 200000)f[i + 1] = f[i] * (i + 1) % MOD;
+    REP(i, 200000)f[i + 1] = f[i] * (i + 1) % mod;
 }
 
-ll nCr(ll n, ll r) {
+ll nCr(ll n, ll r, ll mod = 1e9+7) {
     if (n < r)return 0;
     if (n < 0 or r < 0)return 0;
-    return f[n] * powmod(f[r] * f[n - r], MOD - 2) % MOD;
+    return f[n] * powmod(f[r] * f[n - r], mod - 2) % mod;
 }
 
-ll nPr(ll n, ll r) {
+ll nPr(ll n, ll r, ll mod = 1e9+7) {
     if (n < r)return 0;
     if (n < 0 or r < 0)return 0;
-    return f[n] * powmod(f[n - r], MOD - 2) % MOD;
+    return f[n] * powmod(f[n - r], mod - 2) % mod;
 }
 
-ll nHr(ll n, ll r) {
+ll nHr(ll n, ll r, ll mod = 1e9+7) {
     if (n < r)return 0;
     if (n < 0 or r < 0)return 0;
     return nCr(n + r - 1, r);
--- math/powmod.cpp
+++ math/powmod.cpp
@@ -1,9 +1,9 @@
-ll powmod(ll a, ll b) {
-	a %= MOD;
+ll powmod(ll a, ll b, ll mod = 1e9+7) {
+	a %= mod;
 	ll res = 1;
 	while (b > 0) {
-		if (b % 2 == 1)res = res * a % MOD;
-		a = a * a % MOD;
+		if (b % 2 == 1)res = res * a % mod;
+		a = a * a % mod;
 		b /= 2;
 	}
 	return res;

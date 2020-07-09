a3de20c3e4d354258061a4e290e5ae596ee7439d
kenken714 <kenkenkeiko043@gmail.com>
2020-07-10 00:15:54 +0900

    fixed com.cpp
    fixed

--- math/com.cpp
+++ math/com.cpp
@@ -1,22 +1,32 @@
-ll f[210000];
-void cominit(ll mod = 1e9+7) {
-    f[0] = 1;
-    REP(i, 200000)f[i + 1] = f[i] * (i + 1) % mod;
+vector<ll> f, inv, finv;
+const ll mod = MOD;
+void cominit(ll n = 210000) {
+    f.resize(n + 1);
+    inv.resize(n + 1);
+    finv.resize(n + 1);
+    f[0] = f[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    FOR(i, 2, n + 1) {
+        f[i] = f[i - 1] * i % mod;
+        inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+        finv[i] = finv[i - 1] * inv[i] % mod;
+    }
 }
 
-ll nCr(ll n, ll r, ll mod = 1e9+7) {
+ll nCr(ll n, ll r) {
     if (n < r)return 0;
     if (n < 0 or r < 0)return 0;
-    return f[n] * powmod(f[r] * f[n - r], mod - 2) % mod;
+    return f[n] * (finv[r] * finv[n - r] % mod) % mod;
 }
 
-ll nPr(ll n, ll r, ll mod = 1e9+7) {
+ll nPr(ll n, ll r) {
     if (n < r)return 0;
     if (n < 0 or r < 0)return 0;
-    return f[n] * powmod(f[n - r], mod - 2) % mod;
+    return f[n] * finv[n - r] % mod;
 }
 
-ll nHr(ll n, ll r, ll mod = 1e9+7) {
+ll nHr(ll n, ll r) {
     if (n < r)return 0;
     if (n < 0 or r < 0)return 0;
     return nCr(n + r - 1, r);

e6deea31de5bf48bf2578efef09fcc9c684d1630
kenken714 <kenkenkeiko043@gmail.com>
2020-07-09 23:25:24 +0900

    Create macro.cpp

---
+++ macro.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define REP(i, n) for (ll i = 0; i < n; i++)
+#define REPR(i, n) for (ll i = n; i >= 0; i--)
+#define FOR(i, m, n) for (ll i = m; i < n; i++)
+#define FORR(i, m, n) for (ll i = m; i >= n; i--)
+#define REPO(i, n) for (ll i = 1; i <= n; i++)
+#define ll long long
+#define INF (ll)1 << 60
+#define MINF (-1 * INF)
+#define ALL(n) n.begin(), n.end()
+#define MOD 1000000007
+#define P pair<ll, ll>

b5e695241cdc7c8c995f97eae10d59f7862b613b
kenken714 <kenkenkeiko043@gmail.com>
2020-09-04 02:19:59 +0900

    verify unionfind

--- test/AOJ/DSL_1_A.cpp
+++ test/AOJ/DSL_1_A.cpp
@@ -1,3 +1,17 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_A"
 
-#include 
+#include <bits/stdc++.h>
+#include "../../macro.cpp"
+#include "../../data-str/unionfind/unionfind.cpp"
+
+int main(){
+    int n, q;
+    cin >> n >> q;
+    UnionFind uf(n);
+    REP(i, q){
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a == 0) uf.unionSet(b, c);
+        if (a == 1) cout << uf.findSet(b, c) << endl;
+    }
+}
